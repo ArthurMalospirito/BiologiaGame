308ca6e5914950b2e1627996fd281001ad9b64c6
Arthur Alvarenga Malospirito <arthur.escola.200608@gmail.com>
2026-03-06 00:07:56 -0400

    Adiciona a função de SeekMouse, onde faz o player seguir a direção apontada do mouse na tela com seu eixo de rotação z

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,33 @@
-using System.Collections;
+
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float speed=5;
+
+    private Animator anim;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
-        transform.Rotate(new Vector3(0,0,speed));
+        SeekMouse();
         
+    }
+
+    private void SeekMouse()
+    {
+        Vector2 mouseScreen = Mouse.current.position.ReadValue();
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
+        
+        Vector2 directionToPoint = mouseWorld - transform.position;
+        directionToPoint.Normalize();
+        float rotationAngle = (Mathf.Atan2(directionToPoint.y,directionToPoint.x) * Mathf.Rad2Deg)-90;
+        transform.rotation = Quaternion.Euler(0,0,rotationAngle);
     }
 }
