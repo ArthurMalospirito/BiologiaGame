21bf206915707f50a0183cee764f4c9d5871f3b3
Arthur Alvarenga Malospirito <arthur.escola.200608@gmail.com>
2026-03-06 00:26:35 -0400

    Implementação da função de movimento seguindo o mouse com o clique e força de arrasto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     public float speed=5;
+    private Vector2 direction;
+    private bool CanMove;
+    public float dragForce =1;
 
     private Animator anim;
     private Rigidbody2D rb;
@@ -13,6 +16,7 @@ public class Player : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        rb.linearDamping = dragForce;
     }
     private void Update()
     {
@@ -20,14 +24,39 @@ public class Player : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
     private void SeekMouse()
     {
         Vector2 mouseScreen = Mouse.current.position.ReadValue();
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
         
-        Vector2 directionToPoint = mouseWorld - transform.position;
-        directionToPoint.Normalize();
-        float rotationAngle = (Mathf.Atan2(directionToPoint.y,directionToPoint.x) * Mathf.Rad2Deg)-90;
+        direction = mouseWorld - transform.position;
+        direction.Normalize();
+        float rotationAngle = (Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg)-90;
         transform.rotation = Quaternion.Euler(0,0,rotationAngle);
+    }
+
+    private void Move()
+    {
+        if (CanMove)
+        {
+            rb.linearVelocity = speed * direction;
+        }
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            CanMove=true;
+        }
+        if (context.canceled)
+        {
+            CanMove=false;
+        }
     }
 }
